485641e08d06d7ad3e2e1d847c40a101ba42ee59
Travis Miller <radezorack@gmail.com>
2022-02-01 12:31:46 -0500

    Adding express function

--- functions/index.js
+++ functions/index.js
@@ -30,6 +30,27 @@ admin.initializeApp();
 //   res.json({result: `Message with ID: ${writeResult.id} added.`});
 // });
 
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+// Add middleware to authenticate requests
+// app.use(myMiddleware);
+
+// build multiple CRUD interfaces:
+// app.get('/:id', (req, res) => res.send(Widgets.getById(req.params.id)));
+// app.post('/', (req, res) => res.send(Widgets.create()));
+// app.put('/:id', (req, res) => res.send(Widgets.update(req.params.id, req.body)));
+// app.delete('/:id', (req, res) => res.send(Widgets.delete(req.params.id)));
+app.get('/wolfram_spoken/', (req, res) => console.log("hello"));
+
+// Expose Express API as a single Cloud Function:
+exports.fireAPI = functions.https.onRequest(app);
+
 exports.usermoderator = functions.database.ref('/userInput/{Uid}').onWrite((change) => {
   const before = change.before.val();
   const after = change.after.val();
